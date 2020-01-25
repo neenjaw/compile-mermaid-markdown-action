f7173f9a387458045b101241a7b39ed64ff2319c
Tim Austin <tim@neenjaw.com>
2020-01-24 22:12:20 -0600

    output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -l
 
 outpath=$(realpath "$1")
+printf "Got path: %s\n" "$outpath"
 
 shift $(( OPTIND - 1 ))
 
 for file in "$@"; do
   if [ -f "$file" ]; then
+    printf "Attempting compile of: %s\n" "$file"
     mkdir -p "$outpath"
-
     name=$(basename "$filename" | cut -f 1 -d '.')
-
+    printf "Output to: %s\n" "${outpath}/${name}.png"
     mmdc -i "$file" -o "${outpath}/${name}.png"
   fi
 done
