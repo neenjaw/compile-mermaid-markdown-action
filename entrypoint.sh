e0bfc4cfa4ef210b1e3661e3121545da008810be
Tim Austin <tim@neenjaw.com>
2020-01-24 22:16:13 -0600

    fix path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 outpath=$(realpath "$1")
+mkdir -p "$outpath"
 printf "Got path: %s\n" "$outpath"
 
 shift $(( OPTIND - 1 ))
@@ -8,7 +9,6 @@ shift $(( OPTIND - 1 ))
 for file in "$@"; do
   if [ -f "$file" ]; then
     printf "Attempting compile of: %s\n" "$file"
-    mkdir -p "$outpath"
     name=$(basename "$filename" | cut -f 1 -d '.')
     printf "Output to: %s\n" "${outpath}/${name}.png"
     mmdc -i "$file" -o "${outpath}/${name}.png"
