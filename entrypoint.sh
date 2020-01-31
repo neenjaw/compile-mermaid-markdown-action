3ad285cc7ab705923ac37f5929dede2340c6aaa7
Tim Austin <tim@neenjaw.com>
2020-01-31 00:34:47 -0600

    Big update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,152 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
-outpath="$1"
-mkdir -p "$outpath"
-printf "Got path: %s\n" "$outpath"
+# Script which serves as the entry point to the github action
+# Must receive at least one argument:
+#   -$1       output path, where to place the files created
+#   -$2..$n   files to compile the mermaid markup from
+#
+# Usage:
+#   > entrypoint.sh <output path> [<file1> [<file2> ...]]
 
-shift $(( OPTIND - 1 ))
+# For *.mermaid files, it is compiled and a *.mermaid.png is created at the location
 
-for file in "$@"; do
-  if [ -f "$file" ]; then
-    printf "Attempting compile of: %s\n" "$file"
-    name=$(basename "$file")
-    printf "Output to: %s\n" "${outpath}/${name}.png"
-    /node_modules/.bin/mmdc -p /mmdc/puppeteer-config.json -i "$file" -o "${outpath}/${name}.png"
+# For *.md files:
+#   1) finds all of the mermaid markup in the file
+#   2) creates intermediate files in the output directory *.md.<n>.mermaid where n represents the nth block found
+#   3) compile the mermaid to the directory *.md.<n>.mermaid.png
+#   4) place a reference to the compiled image in the markdown
 
-    if [ ! -f "${outpath}/${name}.png" ]; then
-      printf "Unable to create %s, exiting.." "${outpath}/${name}.png"
-      exit 1
+set -euo pipefail
+
+function main {
+  outpath="${1}"
+  mkdir -p "${outpath}"
+  printf "Got output path: %s\n" "${outpath}"
+
+  shift $(( OPTIND - 1 ))
+
+  for file in "$@"; do
+    if [[ -f "${file}" ]]; then
+      printf "Attempting compile of: %s\n" "${file}"
+
+      file_dirname=$(dirname "${file}")
+      file_basename=$(basename "${file}")
+      file_name="${file_basename%.*}"
+      file_type="${file_basename##*.}"
+
+
+      if [[ "${file_type}" == "mermaid" ]]; then
+
+        output_path="${file_dirname}"
+        output_file="$(dasherize_name ${file_basename}).png"
+        c_mermaid "${file}" "${output_path}/${output_file}"
+
+      elif [[ "${file_type}" == "md" ]]; then
+
+        output_path="${outpath}"
+        c_md_mermaid "${file}" "${output_path}"
+
+      else
+
+        die "*.${file_type} is not a recognized type.  Check that your Github action is submitting a valid file to this entrypoint."
+
+      fi
     fi
+  done
+}
+
+# $1 - the file to compile
+# $2 - the output location
+function c_mermaid {
+  printf "Compiling: %s\n" "${1}"
+  printf "Output to: %s\n" "${2}"
+  /node_modules/.bin/mmdc -p /mmdc/puppeteer-config.json -i "${1}" -o "${2}"
+  confirm_creation "${2}"
+}
+
+# $1 - the file to compile
+# $2 - output path
+function c_md_mermaid {
+  printf "Parsing mermaid codeblocks from %s\n" "${1}"
+
+  input_dir=$(dirname "${1}")
+  dasherized=$(dasherize_name "${1}")
+
+  # Make a temporary directory
+  tmp_dir="${2}/tmp"
+  mkdir -p "${tmp_dir}"
+
+  all_file="${tmp_dir}/mermaid-blocks"
+  block_file="${tmp_dir}/mermaid-block"
+
+  # Get all mermaid blocks
+  sed -n '/^```mermaid/,/^```/ p' < "${1}" > "${all_file}"
+
+  # loop until all mermaid blocks are compiled
+  block_count=0
+  while [[ -s "${all_file}" ]]; do
+    ((block_count=block_count+1))
+
+    # Grab the first block
+    sed '/^```$/q' < "${all_file}" > "${block_file}-${block_count}"    - get the first block
+
+    line_count=$(wc -l < "${block_file}-${block_count}")
+
+    # Drop the first and last line of the block file
+    sed -i '1d;$d' "${block_file}-${block_count}"
+
+    mv "${all_file}" "${all_file}x"
+    sed "1,${line_count}d" < "${all_file}x" > "${all_file}"
+    rm "${all_file}x"
+
+    # "compile file"
+    # TODO: Fixme! Need to actually compile this file...
+    echo "compiled ${block_file}-${block_count}" > "${2}/${dasherized}-${block_count}.png"
+
+    # Compute relative path from the markdown to the tmp_dir
+    relative_path=$(realpath --relative-to="${input_dir}" "${2}/${dasherized}-${block_count}.png")
+
+    # Insert the link to the markdown
+    awk -v n="${block_count}" \
+        -v link="![~mermaid diagram ${block_count}~](${relative_path})" \
+        'BEGIN {i=0}; (/!\[~mermaid diagram/ && i+1 == n) {skip=2}; skip {skip--; next} /```mermaid/ {i++}; (/```mermaid/ && i == n) {print link; print ""}; {print}' \
+        "${1}" > "${1}-temp"
+    rm "${1}"
+    mv "${1}-temp" "${1}"
+  done
+
+  # remove tmp dir
+  rm -rf "${tmp_dir}"
+}
+
+# $1 name to be dasherized
+function dasherize_name {
+  local result=$(echo "${1}" | sed -e 's/\./-/g' | sed -e 's;/;_;g')
+  echo "${result}"
+}
+
+function confirm_creation {
+  if [[ ! -f "${1}" ]]; then
+    die "Unable to create ${1}, exiting."
   fi
+}
+
+function installed {
+  cmd=$(command -v "${1}")
+
+  [[ -n "${cmd}" ]] && [[ -f "${cmd}" ]]
+  return ${?}
+}
+
+function die {
+  >&2 echo "Fatal: ${@}"
+  exit 1
+}
+
+# Check for all required dependencies
+deps=(node awk realpath basename dirname)
+for dep in "${deps[@]}"; do
+  installed "${dep}" || die "Missing '${dep}'"
 done
+
+main "$@"; exit
