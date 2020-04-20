fe9ab940d224171bf9b314c69c5e96e4a9d5283f
Tim Austin <tim@neenjaw.com>
2020-04-20 11:17:32 -0600

    Add absolute image path feature (#3)
    If using this action for github pages, relative links break.  This adds a feature which allows absolute paths to be enabled when links are inserted into the markdown for compiled images.
    
    You can now set the environment variable `ABSOLUTE_IMAGE_LINK` to flag the option:
    - if unset, links remain relative
    - if set, links are not absolute from using the output directory as a directory found in the repository root folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -103,11 +103,18 @@ function c_md_mermaid {
     c_mermaid "${block_file}-${block_count}" "${2}/${dasherized}-${block_count}.png"
 
     # Compute relative path from the markdown to the tmp_dir
-    relative_path=$(realpath --relative-to="${input_dir}" "${2}/${dasherized}-${block_count}.png")
+    image_relative_path=$(realpath --relative-to="${input_dir}" "${2}/${dasherized}-${block_count}.png")
+    image_absolute_path="/${2}/${dasherized}-${block_count}.png"
+
+    if [[ -z "${ABSOLUTE_IMAGE_LINKS}" ]]; then
+      image_path="${image_relative_path}"
+    else
+      image_path="${image_absolute_path}"
+    fi
 
     # Insert the link to the markdown
     awk -v n="${block_count}" \
-        -v rel_path="${relative_path}" \
+        -v path="${image_path}" \
         -v hide_codeblocks="${HIDE_CODEBLOCKS}" \
         -f "${insert_markdown_awk}" \
         "${1}" > "${1}-temp"
